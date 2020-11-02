57fdea7dbe7c86a25967bd893525b565d5e95999
Oleg Tsomkalov <infinitu1327@gmail.com>
2020-11-02 21:14:35 +0200

    Handle null values in AttributeHelpers

--- ShikimoriNET/Helpers/AttributeHelpers.cs
+++ ShikimoriNET/Helpers/AttributeHelpers.cs
@@ -8,21 +8,29 @@ namespace ShikimoriNET.Helpers
     {
         public static string GetParamAttributeData<T>(T value)
         {
+            if (value == null)
+            {
+                return null;
+            }
+        
             var type = value.GetType();
             var field = type.GetField(value.ToString());
-            var attributes =
-                field.GetCustomAttributes(typeof(ParamValueAttribute),
-                    false) as ParamValueAttribute[];
+            var attributes = field.GetCustomAttributes(typeof(ParamValueAttribute), false) as ParamValueAttribute[];
+
             return attributes?.FirstOrDefault()?.Value;
         }
 
         public static string GetDescriptionAttributeData<T>(T value)
         {
+            if (value == null)
+            {
+                return null;
+            }
+            
             var type = value.GetType();
             var field = type.GetField(value.ToString());
-            var attributes =
-                field.GetCustomAttributes(typeof(DescriptionAttribute),
-                    false) as DescriptionAttribute[];
+            var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+            
             return attributes?.FirstOrDefault()?.Description;
         }
     }
